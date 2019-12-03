dc776f3cfad3ec5b71e416be592a8f335f7e4c2b
Alexander Schneider <alexander@schneider.gg>
2019-12-03 09:03:34 +0100

    Solve day2

--- src/advent_of_code_2019/day2/part1.clj
+++ src/advent_of_code_2019/day2/part1.clj
@@ -3,8 +3,8 @@
 
 
 (defn calculate-1202
-  [program]
-  (loop [program program
+  [program noun verb]
+  (loop [program (assoc (assoc program 1 noun) 2 verb)
          pos 0]
     (let [[opcode pos1 pos2 target] (subvec program pos (+ 4 pos))]
       (case opcode
@@ -14,5 +14,16 @@
         (throw Exception)))))
 
 (comment
-  (first (calculate-1202 (input/number-seq-from-file "resources/day2_input.txt" ",")))
+  (first (calculate-1202 (input/number-seq-from-file "resources/day2_input.txt" ",") 12 2))
   )
+
+(defn bruteforce
+  [program]
+  (into {}
+        (for [noun (range 100)
+              verb (range 100)]
+          [(first (calculate-1202 program noun verb)) [noun verb]])))
+
+(def solutions (bruteforce (input/number-seq-from-file "resources/day2_input.txt" ",")))
+
+(def result (+ (* 100 (first (solutions 19690720))) (second (solutions 19690720))))
